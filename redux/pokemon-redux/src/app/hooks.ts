01337aa5ec2f580a1e2c0d2ff52ecc31ba387ee1
Nandan <nandan.jp17@gmail.com>
2024-01-11 14:46:19 -0700

    Updated

--- redux/pokemon-redux/src/app/hooks.ts
+++
@@ -1,5 +0,0 @@
-import { TypedUseSelectorHook, useDispatch, useSelector } from "react-redux";
-import { AppDispath, RootState } from "./store";
-
-export const useAppDispatch = () => useDispatch<AppDispath>;
-export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
